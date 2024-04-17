99b8a74ef236edecb410c4912a35eb3b32463775
NIKITA MALAVIYA <nikitamalaviya47@gmail.com>
2024-04-17 18:17:33 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
+import React from 'react';
 import Navbar from "./Components/Navbar";
-import Form from "./Components/Form";
-import AccordianData from "./Components/AccordianData";
-import { Card } from "@mui/material";
-import Api from "./Components/Api";
-import Addinput from "./Components/Addinput";
+
+import API from './API';
+
 
 
 
 function App() {
   return (
     <>
-
       {/* <Navbar /> */}
-      {/* <div className="row new-form"><Form /></div>
-      <AccordianData />
-      <Card/> */}
-      <Api/>
-      {/* <Addinput/> */}
+      
+      <API/>
     </>
   );
 }
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () =>{
     return(
@@ -12,19 +13,15 @@ const Navbar = () =>{
                     <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                             <li className="nav-item">
-                              <a className="nav-link active" aria-current="page" href="#">Home</a>
+                              <Link to = {'/'} className="nav-link active" aria-current="page" href="#">Home</Link>
                             </li>
                             <li clasclassNames="nav-item">
-                              <a className="nav-link" href="#">Link</a>
+                              <Link to = {'/contact'} className="nav-link" href="#">Contact</Link>
                             </li>
                             <li className="nav-item">
-                              <a className="nav-link disabled">Disabled</a>
+                              <Link to = {'/about us'} className="nav-link ">Anout us</Link>
                             </li>
                         </ul>
-                        <form className="d-flex" role="search">
-                            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-                            <button className="btn btn-outline-success" type="submit">Search</button>
-                        </form>
                     </div>
                 </div>
             </nav>
